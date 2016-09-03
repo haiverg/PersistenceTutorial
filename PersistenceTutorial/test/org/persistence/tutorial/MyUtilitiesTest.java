327bf324ea1921204a435e9f33e7d240f013ce52
haiverg <haiver.galeano@gmail.com>
2016-09-03 11:17:09 -0500

    MyUtilitiesTest in ready

--- PersistenceTutorial/test/org/persistence/tutorial/MyUtilitiesTest.java
+++ PersistenceTutorial/test/org/persistence/tutorial/MyUtilitiesTest.java
@@ -2,13 +2,36 @@ package org.persistence.tutorial;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 public class MyUtilitiesTest {
 
 	@Test
 	public void saveStringToFile() {
-		fail("Not yet implemented");
+		String saveString = "this in a test line one\n" + 
+				"this is test line two\n";
+	File testFile = new File("testsavetostring.txt");	
+	testFile.delete();
+	assertFalse("File should not exist", testFile.exists());
+	
+	assertTrue("File should have been saved", 
+			MyUtilities.saveStringToFile("testsavetostring.txt",saveString));
+	
+	String newString = MyUtilities.getStringFromFile("testsavetostring.txt");
+	assertTrue("Save and get strings should be equal", saveString.equals(newString));
+
+	assertFalse("File should not be saved", MyUtilities.saveStringToFile(
+			"non-existent directory/thisshouldfail",saveString));
+	
+	String emptyString = MyUtilities.getStringFromFile("badfilename.txt");
+	assertTrue("String should be empty", emptyString.length()==0);
+	
+	
+	
+	
+	
 	}
 
 }
