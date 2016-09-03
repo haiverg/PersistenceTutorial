938f07056dce086f4a72ed6d43ae1575cbde0170
haiverg <haiver.galeano@gmail.com>
2016-09-03 12:12:45 -0500

    ???

--- PersistenceTutorial/src/org/persistence/tutorial/MyUtilities.java
+++ PersistenceTutorial/src/org/persistence/tutorial/MyUtilities.java
@@ -7,30 +7,45 @@ public class MyUtilities {
 	public static boolean saveStringToFile(String fileName, String saveString) {
 		boolean saved = false;
 		BufferedWriter bw = null;
-		
+
 		try {
-			bw = new BufferedWriter(
-					new FileWriter(fileName));
+			bw = new BufferedWriter(new FileWriter(fileName));
 			try {
 				bw.write(saveString);
 				saved = true;
-			} 
-			finally {
+			} finally {
 				bw.close();
 			}
-			
+
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-		
+
 		return saved;
-		
+
 	}
 
-	
-	public static String getStringFromFile(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public static String getStringFromFile(String fileName) {
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+			try {
+				String s;
+				while ((s = br.readLine()) != null) {
+					// add lineFeed back since stripped by readLine()
+					sb.append(s);
+					sb.append("\n");
+
+				}
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return sb.toString();
 	}
 
 }
