c62dda73a1e0f2b7bb50a0575a16ae166ae638de
haiverg <haiver.galeano@gmail.com>
2016-09-03 10:15:54 -0500

    Test Java Class for MyUtilities created

---
+++ PersistenceTutorial/test/org/persistence/tutorial/MyUtilitiesTest.java
@@ -0,0 +1,14 @@
+package org.persistence.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyUtilitiesTest {
+
+	@Test
+	public void saveStringToFile() {
+		fail("Not yet implemented");
+	}
+
+}
